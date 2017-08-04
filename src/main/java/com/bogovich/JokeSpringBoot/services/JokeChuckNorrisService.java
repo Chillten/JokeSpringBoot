42f1eebaa5ac933c171b6041be1f4154e2c01e6f
chillten <bogovich.alexander@gmail.com>
2017-08-05 00:22:42 +0700

    Create ChuckNorrisQuotes bean

--- src/main/java/com/bogovich/JokeSpringBoot/services/JokeChuckNorrisService.java
+++ src/main/java/com/bogovich/JokeSpringBoot/services/JokeChuckNorrisService.java
@@ -1,6 +1,7 @@
 package com.bogovich.JokeSpringBoot.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,12 @@ public class JokeChuckNorrisService implements JokeGenerator{
 
     private ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeChuckNorrisService() {
-        chuckNorrisQuotes = new ChuckNorrisQuotes();
+//    public JokeChuckNorrisService() {
+//        chuckNorrisQuotes = new ChuckNorrisQuotes();
+//    }
+
+    public JokeChuckNorrisService(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
